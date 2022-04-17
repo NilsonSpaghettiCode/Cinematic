dfc38a2e2262927fa1604701070befd2e2dc738d
Felipe <anfellr11@mail.com>
2022-04-16 19:05:54 -0500

    Feat: Initial structure for routes were created for a 'create' function

--- app/reservation/src/routes/reservations.js
+++ app/reservation/src/routes/reservations.js
@@ -7,4 +7,23 @@ router.get('/', (req, res) => {
 	res.json({msg: 'Hello world'})
 });
 
+
+// Create Routes
+app.post('/reservations/new', (req, res)=>{
+    console.log(req.body);
+    res.send("Petición post recibida");
+});
+
+app.post('/person/new', (req, res)=>{
+    console.log(req.body);
+    res.send("Petición post recibida");
+});
+
+app.post('/user/new', (req, res)=>{
+    console.log(req.body);
+    res.send("Petición post recibida");
+});
+
+
+
 module.exports = router;
